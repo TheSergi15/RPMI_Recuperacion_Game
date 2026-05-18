92aeb85510903b6f50dfdecee7641857f1ce0d0e
TheSergi15 <sergiovalengamer54@gmail.com>
2026-05-18 13:12:35 +0200

    Update

--- RPMI_Recuperacion_Game/Assets/Scripts/Player.cs
+++ RPMI_Recuperacion_Game/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Player : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
+    public Image healthImage;
 
     private Rigidbody2D rb;
     private bool isGrounded;
@@ -53,6 +55,8 @@ public class Player : MonoBehaviour
         }
 
         SetAnimation(moveInput);
+
+        healthImage.fillAmount = health / 100f;
     }
 
     private void FixedUpdate()
